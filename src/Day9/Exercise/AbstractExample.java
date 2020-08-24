10d5c496a698a23bb6dd766db03453077c29b1e8
SahilTiwari22 <sahiltiwari001@gmail.com>
2020-08-24 18:11:39 +0530

    Day 9 - Exercise 2 solution

---
+++ src/Day9/Exercise/AbstractExample.java
@@ -0,0 +1,26 @@
+package Day9.Exercise;
+
+public class AbstractExample {
+    public static void main(String[] args) {
+        MyBook mb = new MyBook();
+        mb.setTitle("Java Book");
+        System.out.println(mb.getTitle());
+    }
+}
+
+abstract class Book{
+    String title;
+    abstract void setTitle(String s);
+    String getTitle(){
+        return title;
+    }
+
+}
+
+class MyBook extends Book{
+
+    @Override
+    void setTitle(String s) {
+        title = s;
+    }
+}
