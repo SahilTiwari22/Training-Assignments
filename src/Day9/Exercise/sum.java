ab522e052df75369aba7f0021f0908bca833c428
SahilTiwari22 <sahiltiwari001@gmail.com>
2020-08-24 17:48:43 +0530

    Day 9 - Exercise 1 solution

---
+++ src/Day9/Exercise/sum.java
@@ -0,0 +1,31 @@
+package Day9.Exercise;
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Adder add = new Adder();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter first number for addition");
+        int x = sc.nextInt();
+        System.out.println("Please enter second number for addition");
+        int y=sc.nextInt();
+
+        int addition = add.add(x,y);
+        System.out.println("Addition of two numbers is " + addition);
+
+    }
+}
+
+class Arithmetic {
+    int sum;
+    public int add(int a, int b) {
+        return sum = (a + b);
+    }
+
+}
+
+class Adder extends Arithmetic {
+        }
+
+
+
